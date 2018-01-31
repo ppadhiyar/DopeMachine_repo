b2a771daa48d6a53139e38ce2f1669065d7a111c
Parth Padhiyar <23184249+ppadhiyar@users.noreply.github.com>
2018-01-31 18:18:23 -0500

    week2Lab

---
+++ COMP305_Winter2018_week1/Assets/Scripts/clickHammer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickHammer : MonoBehaviour {
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    //Input
+	void Update () {
+		
+	}
+
+    //Guarenteed to run a specific interval
+    // Physics (i.e. things that are time dependent)
+    void FixedUpdate()
+    {
+        
+    } */
+
+    public GameObject batmanDude;
+    public Sprite newImage;
+
+    void Start()
+    {
+       //batmanDude.velocity = new Vector2(5, 0);
+       Rigidbody2D theBody = batmanDude.GetComponent<Rigidbody2D>();
+       theBody.velocity = new Vector2(5, 0);
+    }
+    void Update()
+    {
+        
+    }
+    //Executes when a mouse down event 
+    void OnMouseDown()
+    {
+        //GameObject gObject = GameObject.Find("Batman_Empty_Back");
+        //Debug.Log(batmanDude);
+
+        SpriteRenderer eRend = batmanDude.GetComponent<SpriteRenderer>();
+        eRend.sprite = newImage;
+    }
+}
